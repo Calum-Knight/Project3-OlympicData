e355e03ea76dcef92c0a3c559b8673e05130fac9
Calum Knight <calum_reiss@hotmail.co.uk>
2022-09-22 20:49:15 +0100

    22.09.22 - add js file and update Flask app

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+import psycopg2
+from flask import Flask, render_template
+import user
+from flask import jsonify
+from psycopg2.extras import RealDictCursor
+
+
+
+conn = psycopg2.connect(database="Olympic_db", user=user.username,  
+password = user.password, host="localhost")
+print("connected")
+
+mycursor = conn.cursor(cursor_factory=RealDictCursor)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    mycursor.execute("SELECT * FROM olympic_data where year = 1994")
+    results = mycursor.fetchall()
+    # for x in mycursor:
+    #     print(x)
+    # data = [col for col in mycursor]
+    # mycursor.close()
+    # print(results)
+    return jsonify(results)
+    
+
+    
+    # return render_template('index.html')
+
+# @app.route('/pageone')
+# def PageOne():
+#     mycursor.execute("SELECT * FROM olympic_data")
+#     data = mycursor.fetchall()
+    
+#     return render_template('index.html', data=data)
+
+# @app.route('/pagetwo')
+# def PageTwo():
+#     mycursor.execute("SELECT * FROM olympic_data")
+#     data = mycursor.fetchall()
+#     return render_template('index.html', data=data)
+
+# @app.route('/pagethree')
+# def PageThree():
+#     mycursor.execute("SELECT * FROM olympic_data")
+#     data = mycursor.fetchall()
+#     return render_template('index.html', data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
